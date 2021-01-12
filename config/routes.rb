20ed37e879ab89b47229ae3ab974bcc3d1df12f4
Mayu Miyoshi <mayu.miyoshi1313@gmail.com>
2021-01-12 19:26:59 +0900

    forgot last push

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   get 'tasks', to: 'tasks#list'
+
+  # Create
+  get 'tasks/new', to: 'tasks#new'
+  post "tasks", to: "tasks#create"
+  # Update
+  get    "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  # Read
   get 'tasks/:id', to: 'tasks#show', as: :task
+  # Delete
+  delete "tasks/:id", to: "tasks#destroy"
+
+
 end
